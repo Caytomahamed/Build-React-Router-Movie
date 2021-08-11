e29d708b6cf9a8e7ae6b8f45e5a42ba0fb8a9c36
cayto <aidarous1143@gmail.com>
2021-08-11 18:00:47 +0300

    added MVP note Steps

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import SavedList from './Movies/SavedList';
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link, Router } from "react-router-dom";
 import MovieList from './Movies/MovieList';
 import MovieCard from './Movies/MovieCard'
 export default function App () {
@@ -46,3 +46,79 @@ export default function App () {
     </div>
   );
 }
+
+
+// waxaan ku bartay 
+// steps 
+
+// 1.ina aad soo dajisato rooter m install --save react-router-domReact-Router
+
+//  2.in adigoo jooga indexedDB.js aad soo jiidato Router
+//  import {BrowserRouter as Router} from 'react-router-dom'
+
+// 3.in aad Router add paranet uga dhigto (humiso) app.js 
+// <Router>
+// <App />
+// </Router>
+
+// 4.waxa weeye in aad  soo axios dagsanaya install axios 
+
+// 5.import ku soo samee axios 
+
+// 6.isticmaal axios si aad usoo galiso API-ga 
+
+// axios
+// .get('http://localhost:5000/api/movies')
+// .then(response => {
+//   // Study this response with a console log
+//   // and set the response data to the 'movielist' state
+//   setMovieList(response.data)
+// })
+// .catch(error => {
+//   console.error('Server Error', error);
+// });
+
+// 7.inaad smayso Router soo import ku samayso 
+// import { Switch, Route, Link, Router } from "react-router-dom";
+
+
+// 8.in aad samayso Route adigoo galinaa switch dhexdeeda 
+// switch == waxa meeye muhiimad siinta path ay kaa caaminaysaa (ka ugu qeexida dheerleah ayaa frist-ga ah)
+
+// <switch>
+
+// <Route path="/MovieCard/:id">
+//   <MovieCard   movie={movieList}/>
+// </Route>
+
+//  <Route path='/MovieList'>
+//  <MovieList movies={movieList}/>
+//  </Route>
+// </switch>
+
+
+// 9.in add Route la isticmaayso link si uu kuu geeyo 
+
+// -haddii aad smaynaysi link cusub 
+// <Link to={itsPath} >New link </Link>
+
+// -hadiii uu yahay Element amd Dynamic component ama waxa hore ujirayas 
+
+// {movies.map(movie => (
+//   <Link to={`/MovieCard/${movie.id}`}>
+//   <div className="movie-card">
+//     <h2>{movie.title}</h2>
+//     <div className="movie-director">
+//       Director: <em>{movie.director}</em>
+//     </div>
+//     <div className="movie-metascore">
+//       Metascore: <strong>{movie.metascore}</strong>
+//     </div>
+//   </div>
+//   </Link>
+
+// <Link to='/MovieList'>
+// <div className="home-button">Home</div>
+// </Link>
+
+
